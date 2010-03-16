686f1898b369b09606e5df95352a3df2733d62cb
Sergio Arbeo <serabe@gmail.com>
2010-03-17 00:53:08 +0100

    Added ORG mode

--- serabe.el
+++ serabe.el
@@ -17,21 +17,30 @@
                                 "324" "329" "332" "333" "353" "477"))
 
 ;; ERC Libnotify
-(defun clean-message (s)
-  (setq s (replace-regex-in-string "'" "&apos;"
-          (replace-regex-in-string "\"" "&quot;"
-          (replace-regex-in-string "&" "&amp;"
-          (replace-regex-in-string "<" "&lt;"
-          (replace-regex-in-string ">" "&gt;" s)))))))
+;(defun clean-message (s)
+;  (setq s (replace-regex-in-string "'" "&apos;"
+;          (replace-regex-in-string "\"" "&quot;"
+;          (replace-regex-in-string "&" "&amp;"
+;          (replace-regex-in-string "<" "&lt;"
+;          (replace-regex-in-string ">" "&gt;" s)))))))
 
-(defun call-libnotify (matched-type nick msg)
-  (let* ((cmsg (split-string (clean-message msg)))
-         (nick (first (split-string nick "!")))
-         (msg (mapconcat 'identity (rest cmsg) " ")))
-    (shell-command-to-string
-     (format "notify-send -u critical '%s says:' '%s'" nick msg))))
+;(defun call-libnotify (matched-type nick msg)
+;  (let* ((cmsg (split-string (clean-message msg)))
+;         (nick (first (split-string nick "!")))
+;         (msg (mapconcat 'identity (rest cmsg) " ")))
+;    (shell-command-to-string
+;     (format "notify-send -u critical '%s says:' '%s'" nick msg))))
 
-(add-hook 'erc-text-matched-hook 'call-libnotify)
+;(add-hook 'erc-text-matched-hook 'call-libnotify)
+
+;; ORG mode
+(require 'org-install)
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(add-hook 'org-mode-hook 'turn-on-font-lock)
 
 ;; Function to add paths to clojure classpath.
 ;; Does it works?
